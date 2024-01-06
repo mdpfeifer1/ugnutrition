627f4828486a49aba45843823ea31c52e53a5589
jakobpfeifer135 <138628595+jakobpfeifer135@users.noreply.github.com>
2024-01-05 18:29:21 -0600

    added homepage files

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { FaShoppingCart } from 'react-icons/fa';
-import Logo from '../assets/images/Logo.webp'
+import Logo from '../assets/images/undergroundsportslogo.webp'
 import { useStoreContext } from '../utils/GlobalState';
 import { TOGGLE_CART } from '../utils/actions';
 function NavTabs() {
   const currentPage = useLocation().pathname;
   const [state, dispatch] = useStoreContext();
   return (
-    <section className="bg-[#011638] flex justify-between items-center text-white border-b-4">
+    <section className="bg-[#000000] flex justify-between items-center text-white border-b-4">
       <div className="logo">
         <img className='max-h-[100px] max-w-[200px]' src={Logo} alt="Tech-E" />
       </div>
 
-      <ul className="flex items-center space-x-4 mr-5">
+      <ul className="flex items-center space-x-12 mr-5">
         <li className="nav-item">
           <Link
             to="/"
@@ -28,9 +28,28 @@ function NavTabs() {
             to="/ProductsPage"
             className={`hover:text-gray-300 ${currentPage === '/ProductsPage' ? 'font-bold' : ''}`}
           >
-            Products
+            Shop
           </Link>
         </li>
+
+        <li className="nav-item">
+          <Link
+            to="/ProductsPage"
+            className={`hover:text-gray-300 ${currentPage === '/ProductsPage' ? 'font-bold' : ''}`}
+          >
+            Menu
+          </Link>
+        </li>
+
+        <li className="nav-item">
+          <Link
+            to="/ProductsPage"
+            className={`hover:text-gray-300 ${currentPage === '/ProductsPage' ? 'font-bold' : ''}`}
+          >
+            In Store Only
+          </Link>
+        </li>
+
 
         <li className="nav-item">
           {/* {loggedIn ? (
